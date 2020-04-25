6508880e391f51bf9c37c22d77e2224f8b4fd8b6
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2020-04-25 17:24:14 +0530

    Normal Mode
    A bit more of display work

---
+++ UserManip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+for u in ChiefCommander ArmyGeneral NavyMarshal AirForceChief
+    do
+    sudo useradd --create-home $u
+    echo -e "toor$u\ntoor$u" | sudo passwd $u >/dev/null 2>&1
+done
+
+
+for g in Army Navy AirForce
+    do
+    sudo groupadd $g
+    for n in {1..50}
+        do
+        sudo useradd --create-home -g $g $g$n
+        echo -e "toor$g$n\ntoor$g$n" | sudo passwd $g$n >/dev/null 2>&1
+    done
+done
+
